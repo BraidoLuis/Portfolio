ebf89f06ea51fe48c199be1093983dabdc903cdf
Luís Felipe dos Santos Braido <felipebraido11@outlook.com>
2025-05-28 01:17:52 -0300

    começando página de projetos

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -3,7 +3,7 @@ import Curriculo from "../assets/Currículo - Luís Felipe dos Santos Braido (1)
 
 export default function Home() {
     return (
-        <div className="w-full h-full flex flex-col items-center justify-center px-4 py-8">
+        <div className="w-full h-full flex items-center justify-center px-4 py-8">
             <div className="flex flex-col lg:flex-row items-center justify-center gap-12 flex-wrap">
                 <div className="flex flex-col items-center gap-4">
                     <img
@@ -11,6 +11,21 @@ export default function Home() {
                         className="w-64 h-64 sm:w-80 sm:h-80 object-cover rounded-l-xs"
                         alt="Foto"
                     />
+                    
+                </div>
+
+                <div className="text-[#030000] max-w-2xl bg-white rounded-2xl shadow-xl p-6">
+                    <h1 className="text-3xl font-bold mb-4">Quem sou eu</h1>
+                    <p className="mb-4">
+                        Meu nome é Luís Felipe dos Santos Braido, sou estudante de Engenharia da Computação na UERJ, com previsão de formação para o período 2027.1/2027.2. Tenho experiência em desenvolvimento de software, especialmente com <strong>React.js</strong>, <strong>TypeScript</strong>, <strong>JavaScript</strong>, <strong>HTML</strong> e <strong>CSS</strong>, habilidades adquiridas através de projetos realizados enquanto membro da empresa júnior.
+                    </p>
+                    
+                    <p className="mb-4">
+                        Também possuo experiência e conhecimento em <strong>Python</strong>, adquiridos durante minha participação na Iniciação Científica intitulada <em>"Estudo e Implementação do Método de Correlação de Imagens Digitais usando Software em Python"</em>. Minha função era corrigir o software já existente, tratando bugs e erros identificados durante os experimentos, garantindo seu funcionamento adequado e contribuindo para a continuidade das pesquisas.
+                    </p>
+                    <p>
+                        Atualmente, estou estudando <strong>SQL</strong> nas horas vagas, buscando aprimorar minhas habilidades na gestão e manipulação de dados.
+                    </p>
                     <a href={Curriculo} download className="inline-flex items-center px-4 py-2 bg-black text-white font-bold rounded hover:bg-gray-800 transition duration-200" target="_blank">
                         <svg
                             className="w-4 h-4 mr-2"
@@ -28,33 +43,6 @@ export default function Home() {
                         </svg>
                         Baixar Currículo
                     </a>
-                </div>
-
-                <div className="text-[#030000] max-w-2xl bg-white rounded-2xl shadow-xl p-6">
-                    <h1 className="text-3xl font-bold mb-4">Quem sou eu</h1>
-                    <p className="mb-4">
-                        Meu nome é Luís Felipe dos Santos Braido, sou estudante de Engenharia da Computação na UERJ, com previsão de formação para o período 2027.1/2027.2. Tenho experiência em desenvolvimento de software, especialmente com <strong>React.js</strong>, <strong>TypeScript</strong>, <strong>JavaScript</strong>, <strong>HTML</strong> e <strong>CSS</strong>, habilidades adquiridas através de projetos realizados enquanto membro da empresa júnior.
-                    </p>
-                    <p className="mb-4">
-                        Durante minha trajetória, desenvolvi dois projetos relevantes utilizando React:
-                        <ul className="list-disc pl-6 mt-2">
-                            <li>
-                                <strong>Website para o SPE (Society of Petroleum Engineers) da UERJ:</strong> Um site institucional desenvolvido com React, proporcionando uma interface moderna e interativa para a comunidade acadêmica, com informações sobre eventos, membros e atividades da sociedade.
-                            </li>
-                            <li>
-                                <strong>Portfólio Pessoal:</strong> Um site responsivo que apresenta meus trabalhos, habilidades e projetos, com design moderno e integração com redes sociais.
-                            </li>
-                        </ul>
-                    </p>
-                    <p className="mb-4">
-                        Além disso, participei de um projeto utilizando <strong>WordPress</strong> para o <strong>Programa de Pós-Graduação em Ciência e Tecnologia de Materiais (PPGCTM)</strong> da UERJ. O site institucional foi criado para divulgar informações sobre cursos, pesquisas e eventos, garantindo acessibilidade e responsividade para dispositivos móveis.
-                    </p>
-                    <p className="mb-4">
-                        Também possuo experiência e conhecimento em <strong>Python</strong>, adquiridos durante minha participação na Iniciação Científica intitulada <em>"Estudo e Implementação do Método de Correlação de Imagens Digitais usando Software em Python"</em>. Minha função era corrigir o software já existente, tratando bugs e erros identificados durante os experimentos, garantindo seu funcionamento adequado e contribuindo para a continuidade das pesquisas.
-                    </p>
-                    <p>
-                        Atualmente, estou estudando <strong>SQL</strong> nas horas vagas, buscando aprimorar minhas habilidades na gestão e manipulação de dados.
-                    </p>
                 </div>
             </div>
         </div>
--- src/Screens/Portfolio.tsx
+++ src/Screens/Portfolio.tsx
@@ -1,7 +1,23 @@
-export default function Portfolio () {
+
+
+export default function Home() {
     return (
-        <div className="text-white font-bold text-20px">
-            Projetos
+        <div className="w-full h-full flex items-center justify-center px-4 py-8">
+            <div className="flex flex-col lg:flex-row items-center justify-center gap-12 flex-wrap">
+                
+                <div className="text-[#030000] max-w-2xl bg-white rounded-2xl shadow-xl p-6">
+                    <h1 className="text-3xl font-bold mb-4">Projetos que fiz parte</h1>
+                    <p className="mb-4">
+                        <strong>PPGCTM</strong> - Programa de pós graduação em ciência dos materiais, projeto feito em WordPress, você pode acessar clicando <a href="https://www.ppgctm.iprj.uerj.br/" target="_blank" rel="noopener noreferrer"><strong>Aqui</strong></a>
+                    </p>
+                    <p className="mb-4">
+                        <strong>Serra Energy</strong> - Site para o Evento Serra Energy do SPE Student Chapter UERJ (Society of Petroleum Engineers), projeto feito utilizando React e Tailwind CSS, você pode acessar clicando <a href="https://www.ppgctm.iprj.uerj.br/" target="_blank" rel="noopener noreferrer"><strong>Aqui</strong></a>
+                    </p>
+                    <p>
+                        <strong>SPA Express Cambucas</strong> - Landing page para SPA familiar feito totalmente utilizando React e Tailwind CSS , você pode acessar clicando <a href="https://www.ppgctm.iprj.uerj.br/" target="_blank" rel="noopener noreferrer"><strong>Aqui</strong></a>
+                    </p>
+                </div>
+            </div>
         </div>
-    )
- }
+    );
+}
